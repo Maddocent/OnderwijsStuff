068239390a1002ff57d718fd9c595b39400f98c0
Marc <marc.teunis@hu.nl>
2016-01-14 12:44:20 +0100

    Update SubsettingClass.R

--- SubsettingClass.R
+++ SubsettingClass.R
@@ -1,31 +1,48 @@
+#reading the data
+Dummy_1 <- read.csv("./Data/OnderwijsStuff/Dummy_1.csv", sep=";")
 
-
-Dummy_1 <- read.csv("C:/RWorkingDir/Data/OnderwijsStuff/Dummy_1.csv", sep=";")
-
+#renaming the data.frame
 data <- Dummy_1
 
+#checking structure of 'data'
+str(data)
 
-levels(data$Klas)
+#investigating the first entries
+head(data)
 
-data$Klas <- gsub("cl3", "vl3", data$Klas)
+#checking the levels of "$Class"
+levels(data$Class)
 
-# van character naar factor
-data$Klas <- as.factor(data$Klas)
+#substitute the typo "cl3" for "vl3"
+data$Class <- gsub("cl3", "vl3", data$Class)
+
+# gsub chages the factor Class to "character" the below line changes the "$Class" back to factor
+data$Class <- as.factor(data$Class)
 
 # checking change
 levels(data$Klas)
 
-data$Klas <- droplevels(data$Klas)
-#splitting dat for klas
-Renate_split <- split(data, data$Klas)
+#If there are empty levels remove them with 
+#data$Klas <- droplevels(data$Klas)
 
+#splitting data for Class: this creates a list with i = length(levels(data$Class)) elements
+Class_split <- split(data, data$Class)
+
+# a for-loop to create seperate *.csv files for each level in "$Class". The names of the 
+
+# a function that takes the levels of a factor and prints them out to the console with each element seprated with a comma:
+Labels_Class <- as.character(levels(data$Class))
+
+
+CommaString_Class <- cat(paste(shQuote(Labels_Class, type="cmd"), collapse=", "))
+#copy the output of the line above to "new_names" below
 
 for (i in 1:length(Renate_split)) {
   
   new_names <- c("vl3", "vl4", "vl5")
   
   #the new names are assigned to the data frames
-  assign(new_names[i], Renate_split[[i]])
+  assign(new_names[i], Class_split[[i]])
   
   # save each new data frame as an individual .csv file based on its name using lapply
   
@@ -41,6 +58,8 @@ for (i in 1:length(Renate_split)) {
   print(names(Renate_split))
   
 }
+
+
 
 ##################################
 
